56be6312b585b3e9aad4b18fd94a774424d2d106
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-04 08:53:06 +0000

    added tasks.html and updated app.py to rendeer template

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "randomstring123")
 
-@app.route('/')
-def hello():
-    return "Hello World"
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = "mongodb+srv://lcadmin:Password1@myfirstcluster-y3iip.mongodb.net/task_manager?retryWrites=true&w=majority"
 
-app.run(host=os.getenv("IP", "0.0.0.0"),
-    port=int(os.getenv("PORT", "5000")), debug=True)
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
