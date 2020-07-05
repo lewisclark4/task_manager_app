6aef918850dffb07430060b463f188d3f8a8581f
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-07-05 08:56:54 +0000

    added styling and added edit task function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "randomstring123")
@@ -11,10 +11,11 @@ app.config["MONGO_URI"] = "mongodb+srv://lcadmin:Password1@myfirstcluster-y3iip.
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", 
+    return render_template("tasks.html",
                            tasks=mongo.db.tasks.find())
 
 
@@ -24,7 +25,21 @@ def add_task():
                            categories=mongo.db.categories.find())
 
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
